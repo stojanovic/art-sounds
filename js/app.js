c11083a9bbd72f4bdcba1691a9b1141589eb4936
Slobodan <slobodan@cloudhorizon.com>
2015-06-05 13:48:18 +0200

    Add the sound.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,84 @@
-var request        = new XMLHttpRequest()
-var url            = 'https://random-art.herokuapp.com/'
-var painting       = document.getElementsByClassName('painting')[0]
-var background     = document.getElementsByClassName('background')[0]
-var artTitle       = document.getElementsByClassName('art-title')[0]
-var artCreator     = document.getElementsByClassName('art-creator')[0]
-var artDescription = document.getElementsByClassName('art-description')[0]
-var artLocation    = document.getElementsByClassName('art-location')[0]
-var artImage       = document.getElementsByClassName('art-image')[0]
-var artLink        = document.getElementsByClassName('art-link')[0]
+var audioCtx       = new (window.AudioContext || window.webkitAudioContext)(),
+    request        = new XMLHttpRequest(),
+    url            = 'https://random-art.herokuapp.com/',
+    painting       = document.getElementsByClassName('painting')[0],
+    background     = document.getElementsByClassName('background')[0],
+    artTitle       = document.getElementsByClassName('art-title')[0],
+    artCreator     = document.getElementsByClassName('art-creator')[0],
+    artDescription = document.getElementsByClassName('art-description')[0],
+    artLocation    = document.getElementsByClassName('art-location')[0],
+    artImage       = document.getElementsByClassName('art-image')[0],
+    artLink        = document.getElementsByClassName('art-link')[0],
+    red            = [],
+    green          = [],
+    blue           = [],
+    source         = audioCtx.createBufferSource()
+
+function str2ab(str) {
+  var buf = new ArrayBuffer(str.length * 2) // 2 bytes for each char
+  var bufView = new Uint16Array(buf)
+  for (var i = 0, strLen = str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i)
+  }
+  return buf
+}
+
+// Stereo
+var channels = 3
+// Create an empty two second stereo buffer at the
+// sample rate of the AudioContext
+var frameCount = audioCtx.sampleRate * 2.0
+
+function processImageData(imgData) {
+  var i   = 0,
+      l   = imgData.length,
+      arr = []
+
+  for (; i < l; i = i + 4) {
+    arr.push(((imgData[i] + imgData[i + 1] + imgData[i + 2]) / 3 - 128) / 128)
+    red.push((imgData[i] - 128) / 128)
+    green.push((imgData[i + 1] - 128) / 128)
+    blue.push((imgData[i + 2] - 128) / 128)
+  }
+
+  console.log(red.length)
+
+  var myArrayBuffer = audioCtx.createBuffer(channels, red.length, 22050)
+  var gainNode = audioCtx.createGain()
+
+  for (var channel = 0; channel < channels; channel++) {
+   // This gives us the actual ArrayBuffer that contains the data
+   var nowBuffering = myArrayBuffer.getChannelData(channel)
+   for (var i = 0; i < red.length; i++) {
+    // audio needs to be in [-1.0; 1.0]
+    if (!channel) { 
+      nowBuffering[i] = red[i]
+    } else if (channel === 1) {
+      nowBuffering[i] = green[i]
+    } else {
+      nowBuffering[i] = blue[i]
+    }
+   }
+  }
+
+  // Get an AudioBufferSourceNode.
+  // This is the AudioNode to use when we want to play an AudioBuffer
+  var source = audioCtx.createBufferSource()
+  // set the buffer in the AudioBufferSourceNode
+  source.buffer = myArrayBuffer
+  // connect the AudioBufferSourceNode to the
+  // destination so we can hear the sound
+  source.connect(audioCtx.destination)
+  source.connect(gainNode)
+  // start the source playing
+  source.start()
+
+  /*console.log(red)
+  console.log(green)
+  console.log(blue)*/
+}
 
 function getImageData(img) {
-  console.log(img, img.width, img.height)
   var canvas    = document.createElement('canvas')
   canvas.width  = img.width
   canvas.height = img.height
@@ -28,10 +96,9 @@ request.onreadystatechange = function() {
     painting.src = art.image
     painting.crossOrigin = 'Anonymous'
 
-    var imgData
     painting.addEventListener('load', function() {
-      imgData = getImageData(painting)
-      console.log(imgData)
+      var imgData = getImageData(painting)
+      processImageData(imgData.data)
     })
 
     artTitle.innerHTML         = art.title
